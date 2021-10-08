1a20981fd5970539c47699a8ae57ea651a0869ea
njw9108 <89832278+njw9108@users.noreply.github.com>
2021-10-08 11:45:03 +0900

    Initial commit

---
+++ lib/dart_quiz.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+class Trader {
+  String name;
+  String city;
+
+  Trader(this.name, this.city);
+}
+
+class Transaction {
+  Trader trader;
+  int year;
+  int value;
+
+  Transaction(this.trader, this.year, this.value);
+}
+
+final transactions = [
+  Transaction(Trader("Brian", "Cambridge"), 2011, 300),
+  Transaction(Trader("Raoul", "Cambridge"), 2012, 1000),
+  Transaction(Trader("Raoul", "Cambridge"), 2011, 400),
+  Transaction(Trader("Mario", "Milan"), 2012, 710),
+  Transaction(Trader("Mario", "Milan"), 2012, 700),
+  Transaction(Trader("Alan", "Cambridge"), 2012, 950),
+];
+
+void main() {
+// 1. 2011년에 일어난 모든 트랜잭션을 찾아 값을 오름차순으로 정리하여 나열하시오
+  (transactions..sort((a, b) => a.value.compareTo(b.value)))
+      .forEach((e) => print(e.value));
+  print('');
+// 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
+  Set<String> city = {};
+  transactions.forEach((e) => city.add(e.trader.city));
+  city.forEach((e) => print(e));
+  print('');
+// 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
+  Set<String> cambridgeWorker = {};
+  transactions
+      .where((e) => (e.trader.city == 'Cambridge'))
+      .forEach((e) => cambridgeWorker.add(e.trader.name));
+  (cambridgeWorker.toList()..sort((a, b) => a.compareTo(b))).forEach(print);
+  print('');
+// 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
+  Set<String> name = {};
+  transactions.forEach((e) => name.add(e.trader.name));
+  (name.toSet().toList()..sort((a, b) => a.compareTo(b))).forEach(print);
+  print('');
+// 5. 밀라노에 거래자가 있는가?
+  print(transactions.any((e) => e.trader.city == 'Milan'));
+  print('');
+// 6. 케임브리지에 거주하는 거래자의 모든 트랙잭션값을 출력하시오
+  transactions.where((e) => e.trader.city == 'Cambridge').toList().forEach((e) {
+    print(e.value);
+  });
+  print('');
+// 7. 전체 트랜잭션 중 최대값을 얼마인가?
+  List<int> valueItems = [];
+  transactions.forEach((e) => valueItems.add(e.value));
+  print(valueItems.reduce((v, e) => max(v,e)));
+  print('');
+// 8. 전체 트랜잭션 중 최소값은 얼마인가?
+  print(valueItems.reduce((v, e) => min(v,e)));
+  print('');
+}
