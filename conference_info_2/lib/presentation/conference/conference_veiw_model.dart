2b1793fd179c02f78cb66272d6a35c9800f848ec
njw9108 <89832278+njw9108@users.noreply.github.com>
2022-01-16 16:43:36 +0900

    view model 작성

---
+++ conference_info_2/lib/presentation/conference/conference_veiw_model.dart
@@ -0,0 +1,22 @@
+import 'package:conference_info_2/domain/model/conference_info.dart';
+import 'package:conference_info_2/domain/use_case/get_conference_info_use_case.dart';
+import 'package:flutter/material.dart';
+
+class ConferenceViewModel with ChangeNotifier {
+  final GetConferenceInfoUseCase getConferenceInfo;
+  List<ConferenceInfo> conferenceInfo = [];
+
+  ConferenceViewModel(this.getConferenceInfo) {
+    fetchInfo();
+  }
+
+  Future<void> fetchInfo() async {
+    final result = await getConferenceInfo();
+    result.when(success: (info) {
+      conferenceInfo = info;
+    }, error: (message) {
+      print(message);
+    });
+    notifyListeners();
+  }
+}
