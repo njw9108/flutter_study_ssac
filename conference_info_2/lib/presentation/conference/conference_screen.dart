d1ef6e5b951beb4655b2c735b91ddc7fb6c03cd8
njw9108 <89832278+njw9108@users.noreply.github.com>
2022-01-16 16:51:35 +0900

    conference screen 정의

---
+++ conference_info_2/lib/presentation/conference/conference_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConferenceScreen extends StatelessWidget {
+  const ConferenceScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
