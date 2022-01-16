b80138d3fd6be65ec61d8c466be4342a7ccbe879
njw9108 <89832278+njw9108@users.noreply.github.com>
2022-01-16 16:59:59 +0900

    state 추가

---
+++ conference_info_2/lib/presentation/conference/conference_state.dart
@@ -0,0 +1,16 @@
+import 'package:conference_info_2/domain/model/conference_info.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'conference_state.freezed.dart';
+
+part 'conference_state.g.dart';
+
+@freezed
+class ConferenceState with _$ConferenceState {
+  factory ConferenceState({
+    @Default([]) List<ConferenceInfo> conferenceInfo,
+  }) = _ConferenceState;
+
+  factory ConferenceState.fromJson(Map<String, dynamic> json) =>
+      _$ConferenceStateFromJson(json);
+}
