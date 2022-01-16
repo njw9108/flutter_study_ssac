c4a9ff68940465aee9b85314a4dd3e622773c272
njw9108 <89832278+njw9108@users.noreply.github.com>
2022-01-16 16:21:38 +0900

    return 형식 변경

--- conference_info_2/lib/domain/repository/conference_info_repository.dart
+++ conference_info_2/lib/domain/repository/conference_info_repository.dart
@@ -2,5 +2,5 @@ import 'package:conference_info_2/core/result.dart';
 import 'package:conference_info_2/domain/model/conference_info.dart';
 
 abstract class ConferenceInfoRepository {
-  Future<Result<ConferenceInfo>> getConferenceInfo();
+  Future<Result<List<ConferenceInfo>>> getConferenceInfo();
 }
